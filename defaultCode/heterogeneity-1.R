887cae620555ff8a45f353adbca4158110733755
juliabh <30322584+juliabh@users.noreply.github.com>
2024-11-08 16:15:52 +0000

    Update heterogeneity-1.R female for women
    change terminology to use men and women instead of male and female. All variables ending in _f now end in _w

--- defaultCode/heterogeneity-1.R
+++ defaultCode/heterogeneity-1.R
@@ -1,43 +1,43 @@
 ##Differential equations
 
-deriv(S_f) <- -betaMW_1c*pcr_1c*S_f*I_m/N_m + recovery_1c*I_f
-deriv(S_m) <- -betaWM_1c*pcr_1c*S_m*I_f/N_f + recovery_1c*I_m
+deriv(S_w) <- -betaMW_1c*pcr_1c*S_w*I_m/N_m + recovery_1c*I_w
+deriv(S_m) <- -betaWM_1c*pcr_1c*S_m*I_w/N_w + recovery_1c*I_m
 
-deriv(I_f) <- betaMW_1c*pcr_1c*S_f*I_m/N_m - recovery_1c*I_f
-deriv(I_m) <- betaWM_1c*pcr_1c*S_m*I_f/N_f - recovery_1c*I_m
+deriv(I_w) <- betaMW_1c*pcr_1c*S_w*I_m/N_m - recovery_1c*I_w
+deriv(I_m) <- betaWM_1c*pcr_1c*S_m*I_w/N_w - recovery_1c*I_m
 
 
 ##Initial conditions
 
-initial(S_f) <- 99990
+initial(S_w) <- 99990
 initial(S_m) <- 99990
 
-initial(I_f) <- 10
+initial(I_w) <- 10
 initial(I_m) <- 10
 
 
 ##Other equations
 
-N_f <- S_f+I_f
+N_w <- S_w+I_w
 N_m <- S_m+I_m
-N_all <- N_f+N_m
+N_all <- N_w+N_m
 
 
 
 ##Parameter values
 
 pcr_1c <-user(2)		#partner change rate
-betaMW_1c <- user(0.5)  	#per partner STI transmission probability, male to female
-betaWM_1c <- user(0.5)  	#per partner STI transmission probability, female to male
+betaMW_1c <- user(0.5)  	#per partner STI transmission probability, men to women
+betaWM_1c <- user(0.5)  	#per partner STI transmission probability, women to men
 recovery_1c <- user(2)          #recovery rate of infection per person per year (1/mean duration in years)		
 
 
 ##Additional outputs
 
-output(PopSize) <- N_f+N_m
-output(prevalence_f) <- I_f/N_f
+output(PopSize) <- N_w+N_m
+output(prevalence_w) <- I_w/N_w
 output(prevalence_m) <- I_m/N_m
-output(prevalence_all) <- (I_f+I_m)/N_all
-output(newInf_f) <- betaMW_1c*pcr_1c*S_f*I_m/N_m
-output(newInf_m) <- betaWM_1c*pcr_1c*S_m*I_f/N_f
-output(newInf_all) <- betaMW_1c*pcr_1c*S_f*I_m/N_m + betaWM_1c*pcr_1c*S_m*I_f/N_f
+output(prevalence_all) <- (I_w+I_m)/N_all
+output(newInf_w) <- betaMW_1c*pcr_1c*S_w*I_m/N_m
+output(newInf_m) <- betaWM_1c*pcr_1c*S_m*I_w/N_w
+output(newInf_all) <- betaMW_1c*pcr_1c*S_w*I_m/N_m + betaWM_1c*pcr_1c*S_m*I_w/N_w
